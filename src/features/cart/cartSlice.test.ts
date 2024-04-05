4cd4d4dd4304365de01721188db6d012ed1d27e7
Grant Johnson <52875086+grantmj@users.noreply.github.com>
2024-04-05 02:30:40 -0700

    Add files via upload
    part 14, I realized I have an error for my directory for some reason.  Need to fix it, but for some reason code goes through, but is wrong in lesson directory.

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -10,7 +10,7 @@ import cartReducer, {
     getMemoizedNumItems,
     getTotalPrice
 } from "./cartSlice";
-import type { RootState } from "../../app/store";
+import { RootState, getStoreWithState } from "../../app/store";
 import  products from "../../../public/products.json"
 import * as api from "../../app/api";
 
@@ -286,4 +286,36 @@ describe("selectors", () => {
             expect(actions[1].error.message).toEqual('Must include cart items');
         });
     });
+    describe("thunks", () => {
+        describe("checkoutCart w/full redux store", () => {
+          it("should checkout with items", async () => {
+            const state = getStateWithItems({ testItem: 3 });
+            const store = getStoreWithState(state);
+            await store.dispatch(checkoutCart());
+            expect(store.getState().cart).toEqual({
+              items: {},
+              errorMessage: "",
+              checkoutState: "READY"
+            })
+          });
+          it("should fail with no items", async () => {
+            const state = getStateWithItems({});
+            const store = getStoreWithState(state);
+            await store.dispatch(checkoutCart());
+            expect(store.getState().cart).toEqual({
+              items: {},
+              checkoutState: "ERROR",
+              errorMessage: "Must include cart items"
+            })
+          });
+        });
+      });
+      
+      function getStateWithItems(items: Record<string, number>): RootState {
+        const state: RootState = {
+          products: { products: {} },
+          cart: { errorMessage: "", checkoutState: "READY", items }
+        }
+        return state
+      }
   });
