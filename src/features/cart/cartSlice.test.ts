01c14b608beae050aed03b6007699ff8a251afd5
Grant Johnson <52875086+grantmj@users.noreply.github.com>
2024-04-05 02:59:16 -0700

    Add files via upload
    Part 15.  Essentially checking if we have an empty cart or not.

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -24,6 +24,7 @@ jest.mock("../../app/api" , () => {
         async checkout(items: api.CartItems ={}){
             const empty = Object.keys(items).length ===0;
             if(empty) throw new Error("Must include cart items");
+            if(items.evilItem > 0) throw new Error();
             if(items.badItem > 0) return { success: false};
             return { success: true};
         }
@@ -293,7 +294,7 @@ describe("selectors", () => {
             const store = getStoreWithState(state);
             await store.dispatch(checkoutCart());
             expect(store.getState().cart).toEqual({
-              items: {},
+              items: { badItem: 7},
               errorMessage: "",
               checkoutState: "READY"
             })
@@ -307,6 +308,36 @@ describe("selectors", () => {
               checkoutState: "ERROR",
               errorMessage: "Must include cart items"
             })
+          });
+          it("should handle an error message", async () => {
+            const state = getStateWithItems({ badItem: 7 });
+            const store = getStoreWithState(state);
+            await store.dispatch(checkoutCart());
+            expect(store.getState().cart).toEqual({
+              items: { badItem: 7 },
+              checkoutState: "ERROR",
+              errorMessage: ""
+            });
+          });
+          it("should handle an empty error message", async () => {
+            const state = getStateWithItems({ evilItem: 7 });
+            const store = getStoreWithState(state);
+            await store.dispatch(checkoutCart());
+            expect(store.getState().cart).toEqual({
+              items: { evilItem: 7 },
+              checkoutState: "ERROR",
+              errorMessage: ""
+            });
+          });
+          it("should be pending before checking out", async () => {
+            const state = getStateWithItems({ evilItem: 7 });
+            const store = getStoreWithState(state);
+            expect(store.getState().cart.checkoutState).toEqual("READY");
+            const action =  store.dispatch(checkoutCart());
+            expect(store.getState().cart.checkoutState).toEqual("LOADING");
+            await action;
+            expect(store.getState().cart.checkoutState).toEqual("READY");
+            });
           });
         });
       });
