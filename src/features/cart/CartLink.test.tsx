3c6a42343251c1d05a399c51cffe37941453b5b0
Grant Johnson <52875086+grantmj@users.noreply.github.com>
2024-04-05 04:08:08 -0700

    Add files via upload
    Part 20, was just applying our helpers.

--- src/features/cart/CartLink.test.tsx
+++ src/features/cart/CartLink.test.tsx
@@ -1,46 +1,32 @@
 
 import React from "react";
-import { render, screen} from "@testing-library/react";
-import{ CartLink } from "./CartLink";
-import { Provider } from "react-redux";
-import { store } from "../../app/store";
+import { screen } from "@testing-library/react";
+import { CartLink } from "./CartLink";
 import { addToCart, updateQuantity, removeFromCart } from "./cartSlice";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import { renderWithContext, getStateWithItems } from "../../test-utils";
 
 test("should contain a link", () => {
-    render(<CartLink />);
-    expect(screen.getByRole("link")).toBeInTheDocument();
-
+  renderWithContext(<CartLink />);
+  expect(screen.getByRole("link")).toBeInTheDocument();
 });
 
-test("should show text when ther are no items", () => {
-    renderWithContext(<CartLink />);
-    const link = screen.getByRole("link");
-    expect(link).toHaveTextContent("Cart");
-    expect(link).not.toHaveTextContent("0");
-    expect(link).not.toHaveTextContent("1");
+test("should show text when there are no items", () => {
+  renderWithContext(<CartLink />);
+  const link = screen.getByRole("link");
+  expect(link).toHaveTextContent("Cart");
+  expect(link).not.toHaveTextContent("0");
+  expect(link).not.toHaveTextContent("1");
 });
-
 test("should show the correct number of items", () => {
-    store.dispatch(addToCart("testItem"));
-    renderWithContext(<CartLink />);
-    const link = screen.getByRole("link");
-    expect(link).toHaveTextContent("1");
-    store.dispatch(updateQuantity({ id : "testItem", quantity: 5}));
-    expect(link).toHaveTextContent("5");
-    store.dispatch(addToCart("anotherItem"));
-    expect(link).toHaveTextContent("6");
-    store.dispatch(removeFromCart("testItem"));
-    store.dispatch(removeFromCart("anotherItem"));
-    expect(link).toHaveTextContent("Cart");
-
+  const state = getStateWithItems({ testItem: 1 });
+  const { store } = renderWithContext(<CartLink />, state);
+  const link = screen.getByRole("link");
+  expect(link).toHaveTextContent("1");
+  store.dispatch(updateQuantity({ id: "testItem", quantity: 5 }));
+  expect(link).toHaveTextContent("5");
+  store.dispatch(addToCart("anotherItem"));
+  expect(link).toHaveTextContent("6");
+  store.dispatch(removeFromCart("testItem"));
+  store.dispatch(removeFromCart("anotherItem"));
+  expect(link).toHaveTextContent("Cart");
 });
-
-function renderWithContext(element: React.ReactElement) {
-    render(
-    <Provider store={store}>
-        <Router>{element}</Router>
-        </Provider> 
-    );
-}
