4794a94f72f1249eb3a2dd925f3246ecd0129841
Grant Johnson <52875086+grantmj@users.noreply.github.com>
2024-04-04 23:33:37 -0700

    Add files via upload
    Part 11.  Just inputting the api helper.

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -8,7 +8,26 @@ import cartReducer, {
     getTotalPrice
 } from "./cartSlice";
 import type { RootState } from "../../app/store";
-import  products from "../../../public/products.json";
+import  products from "../../../public/products.json"
+import * as api from "../../app/api";
+
+jest.mock("../../app/api" , () => {
+    return {
+        async getProducts(){
+            return [];
+        },
+        async checkout(items: api.CartItems ={}){
+            const empty = Object.keys(items).length ===0;
+            if(empty) throw new Error("Must include cart items");
+            if(items.badItem > 0) return { success: false};
+            return { success: true};
+        }
+    }
+})
+
+test("checkout should work", async () => {
+    await api.checkout({ fakeItem: 4 });
+});
 
 describe("cart reducer", () => {
     test("an empty action", () => {
