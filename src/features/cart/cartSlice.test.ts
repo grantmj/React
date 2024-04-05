e1401c7cacd92037cd3a7bebebeb7847813e097d
Grant Johnson <52875086+grantmj@users.noreply.github.com>
2024-04-04 23:11:28 -0700

    Add files via upload
    Part 10.  Started unit testing for part 9, was overall not bad, just initially 3 of my test weren't working.

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -4,10 +4,11 @@ import cartReducer, {
     removeFromCart, 
     updateQuantity, 
     getNumItems,
-    getMemoizedNumItems
+    getMemoizedNumItems,
+    getTotalPrice
 } from "./cartSlice";
 import type { RootState } from "../../app/store";
-
+import  products from "../../../public/products.json";
 
 describe("cart reducer", () => {
     test("an empty action", () => {
@@ -117,5 +118,84 @@ describe("selectors", () => {
         getMemoizedNumItems({ cart } as RootState);
         expect(getMemoizedNumItems.recomputations()).toEqual(2);
       });
+    })
+    describe("getTotalPrice", () => {
+        it("should return 0 with an empty cart"   , () => {
+            const state: RootState={
+                cart: { checkoutState: "READY", errorMessage: "", items: {}},
+                products: { products: {}}
+            }
+            const result = getTotalPrice(state);
+            expect(result).toEqual("0.00");
+        });
+        it("should not compute again with the same state"   , () => {
+            const state: RootState={
+                cart: { checkoutState: "READY", errorMessage: "", 
+                items: {
+                    [products[0].id]: 3,
+                    [products[1].id]: 4
+                }},
+                products: { products: {
+                    [products[0].id]: products[0],
+                    [products[1].id]: products[1],
+                    }
+                }
+            }
+            getTotalPrice.resetRecomputations();
+            const result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+            getTotalPrice(state);
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+        });
+        it("should recompute with new products"   , () => {
+            const state: RootState={
+                cart: { checkoutState: "READY", errorMessage: "", 
+                items: {
+                    [products[0].id]: 3,
+                    [products[1].id]: 4
+                }},
+                products: { products: {
+                    [products[0].id]: products[0],
+                    [products[1].id]: products[1],
+                    }
+                }
+            }
+            getTotalPrice.resetRecomputations();
+            let result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+            state.products.products = {
+                [products[0].id]: products[0],
+                [products[1].id]: products[1],
+                [products[2].id]: products[2],
+            }
+            result = getTotalPrice({ ...state});
+            expect(result).toEqual("43.23");
+            expect(getMemoizedNumItems.recomputations()).toEqual(2);
+        });
+        it("should recompute when cart changes", () => {
+            const state: RootState={
+                cart: { checkoutState: "READY", errorMessage: "", 
+                items: {
+                    [products[0].id]: 3,
+                    [products[1].id]: 4
+                }},
+                products: { products: {
+                    [products[0].id]: products[0],
+                    [products[1].id]: products[1],
+                    }
+                }
+            }
+            getTotalPrice.resetRecomputations();
+            let result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+            state.cart.items = {};
+            result = getTotalPrice({ ...state});
+            expect(result).toEqual("0.00");
+            expect(getMemoizedNumItems.recomputations()).toEqual(2);
+        });
+
     });
   });
