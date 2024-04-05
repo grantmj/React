4827cb69a756079d41e7c76a34ab153d73b99f94
Grant Johnson <52875086+grantmj@users.noreply.github.com>
2024-04-04 21:34:16 -0700

    Add files via upload
    Part 9, tests 8/8.  Starting to understand the concept of memoization.  Wasn't the hardest to implement, was just understanding the concept.  Essentially this function is used to save the work that has already been done to avoid doing it again.

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -1,4 +1,11 @@
-import cartReducer, { CartState, addToCart, removeFromCart, updateQuantity, getNumItems } from "./cartSlice";
+import cartReducer, { 
+    CartState, 
+    addToCart, 
+    removeFromCart, 
+    updateQuantity, 
+    getNumItems,
+    getMemoizedNumItems
+} from "./cartSlice";
 import type { RootState } from "../../app/store";
 
 
@@ -62,28 +69,53 @@ describe("cart reducer", () => {
 });
 
 describe("selectors", () => {
-    describe("getNumItems", () => {
-        it("should return 0 with no items", () => {
-            const cart: CartState = {
-                checkoutState: "READY",
-                errorMessage: "",
-                items: {}
-            };
-            // construct a root state here:
-            
-            const result = getNumItems({ cart } as RootState);
-            expect(result).toEqual(0);
-        });
-        it("should add up the total", () => {
-            const cart: CartState = {
-                checkoutState: "READY",
-                errorMessage: "",
-                items: { abc: 3, def: 3 }
-            };
-            // construct a another root state here:
-            
-            const result = getNumItems({ cart } as RootState);
-            expect(result).toEqual(6);
-        });
+    describe("getMemoizedNumItems", () => {
+      it("should return 0 with no items", () => {
+        const cart: CartState = {
+          checkoutState: "READY",
+          errorMessage: "",
+          items: {},
+        };
+        const result = getMemoizedNumItems({ cart } as RootState);
+        expect(result).toEqual(0);
+      });
+      it("should add up the total", () => {
+        const cart: CartState = {
+          checkoutState: "READY",
+          errorMessage: "",
+          items: { abc: 3, def: 3 },
+        };
+        const result = getMemoizedNumItems({ cart } as RootState);
+        expect(result).toEqual(6);
+      });
+      it("should not compute again with the same state", () => {
+        const cart: CartState = {
+          checkoutState: "READY",
+          errorMessage: "",
+          items: { abc: 3, def: 3 },
+        };
+        getMemoizedNumItems.resetRecomputations();
+        getMemoizedNumItems({ cart } as RootState);
+        expect(getMemoizedNumItems.recomputations()).toEqual(1);
+        getMemoizedNumItems({ cart } as RootState);
+        expect(getMemoizedNumItems.recomputations()).toEqual(1);
+        getMemoizedNumItems({ cart } as RootState);
+        getMemoizedNumItems({ cart } as RootState);
+        getMemoizedNumItems({ cart } as RootState);
+        expect(getMemoizedNumItems.recomputations()).toEqual(1);
+      });
+      it("should recompute with new state", () => {
+        const cart: CartState = {
+          checkoutState: "READY",
+          errorMessage: "",
+          items: { abc: 3, def: 3 },
+        };
+        getMemoizedNumItems.resetRecomputations();
+        getMemoizedNumItems({ cart } as RootState);
+        expect(getMemoizedNumItems.recomputations()).toEqual(1);
+        cart.items = { abc: 2 };
+        getMemoizedNumItems({ cart } as RootState);
+        expect(getMemoizedNumItems.recomputations()).toEqual(2);
+      });
     });
-});
+  });
